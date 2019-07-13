a7a10656b5ac3b36ccd530d7f3ca7f468729c305
vijay shekhar <vijay.shekhar463@gmail.com>
2019-07-13 12:01:49 +0530

    Update ann.py

--- ann.py
+++ ann.py
@@ -70,47 +70,11 @@ y_pred = (y_pred >0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-"""data visualisations"""
-
-
-#training set visualisations
-from matplotlib.colors import ListedColormap
-X_set, y_set = x_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-#test set visualisations
-X_set, y_set = x_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
 """ test case to check whether the customer will leave thebank or not:"""
 
-custom_pred = classifier.predict(sc.transform(np.array([[0.0, 0, 300, 1, 39, 2, 453400, 3, 1, 1, 3000]])))
+custom_pred = classifier.predict(
+  .transform(np.array([[0.0, 0, 300, 1, 39, 2, 453400, 3, 1, 1, 3000]])))
 custom_pred = (custom_pred > 0.5)
 
 #evaluating the ann
